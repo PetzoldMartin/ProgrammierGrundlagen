6b17003ffdd5bc745c87814d0e630b2ef2434ee2
map111ks <map111ks@b932b21e-e20f-4f26-a6f4-977a90250f57>
2012-03-31 15:28:24 +0000

    Vorbereitung zur Ordnerubenennung

---
+++ Hausaufgaben/Hausaufgabe12/trunk/src/Hausaufgabe12/Box.java
@@ -0,0 +1,9 @@
+package Hausaufgabe12;
+//Die Klasse die einen Karton erzeugt
+public class Box {
+ private int width;//Die Breite des Kartons
+ private int height;//Die Höhe des Kartons
+ private 
+ 
+
+}
