4d0fa1372b1e15bad443c124e40cb7e1eb963a15
map111ks <map111ks@b932b21e-e20f-4f26-a6f4-977a90250f57>
2012-04-03 19:23:46 +0000

    Vorläufig Kommentiert

--- Hausaufgaben_Martin_Petzold/Hausaufgabe12_Martin_Petzold/trunk/src/Hausaufgabe12/Box.java
+++ Hausaufgaben_Martin_Petzold/Hausaufgabe12_Martin_Petzold/trunk/src/Hausaufgabe12/Box.java
@@ -4,10 +4,15 @@ package Hausaufgabe12;
 public class Box {
 	private double width;// Die Breite des Kartons
 	private double height;// Die Höhe des Kartons
-	private double length;// DIe Länge des Kartons
+	private double length;// Die Länge des Kartons
 
 	/*
 	 * Konstruktor zum erstellen eines Kartons
+	 */
+	/**
+	 * @param width  Die zu Übergebende Breite des Kartons
+	 * @param heigth Die zu Übergebende Höhe des Kartons
+	 * @param length Die zu Übergebende Länge des Kartons
 	 */
 	public Box(double width, double heigth, double length) {
 		this.width = width;
@@ -18,48 +23,81 @@ public class Box {
 	/*
 	 * Konstruktor zur erstellung eines Würfelförmigen Kartons
 	 */
+	/**
+	 * @param side Die zu Übergebende Seitenlänge des Würfelförmigen Kartons
+	 */
 	public Box(double side) {
 		this.width = this.height = this.length = side;
 	}
 
+	/**
+	 * @param oldBox Der zu Übergebende Karton
+	 */
 	public Box(Box oldBox) {
 		this.width = oldBox.getWidth();
 		this.height = oldBox.getHeight();
 		this.length = oldBox.getLength();
 	}
 
+	/**
+	 * @return die Breite
+	 */
 	public double getWidth() {
 		return width;
 	}
 
+	/**
+	 * @param width die neue Breite
+	 */
 	public void setWidth(double width) {
 		this.width = width;
 	}
 
+	/**
+	 * @return die Höhe
+	 */
 	public double getHeight() {
 		return height;
 	}
 
+	/**
+	 * @param height die neue Höhe
+	 */
 	public void setHeight(double height) {
 		this.height = height;
 	}
 
+	/**
+	 * @return die Länge
+	 */
 	public double getLength() {
 		return length;
 	}
 
+	/**
+	 * @param length die neue Länge
+	 */
 	public void setLength(double length) {
 		this.length = length;
 	}
 
+	/**
+	 * @return das errechnete Volumen des Kartons
+	 */
 	public double calculateVolume() {
 		return (width * height * length);
 	}
 
+	/**
+	 * @return der errechnete Oberflächeninhalt des Kartons
+	 */
 	public double calculateSurfaceArea() {
 		return (2 * (width * height + width * length + height * length));
 	}
 
+	/**
+	 * @return der um 25% vergrößerte Karton
+	 */
 	public Box getBiggerBox() {
 		Box biggerBox = new Box(this);
 		double faktor = 1.25;
@@ -69,6 +107,10 @@ public class Box {
 		return biggerBox;
 	}
 
+	/**
+	 * @param otherbox der Karton in dem dieser Karton Eingesetzt wird
+	 * @return true wenn der Karton ungedreht in den anderen hineinpasst
+	 */
 	public boolean FitsIn(Box otherbox) {
 		if (otherbox.getHeight() > this.getHeight()) {
 			if (otherbox.getLength() > this.getLength()) {
