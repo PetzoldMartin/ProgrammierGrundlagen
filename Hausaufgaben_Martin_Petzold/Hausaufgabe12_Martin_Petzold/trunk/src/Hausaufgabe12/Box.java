47eb1514e7ddfe48274c50d52c97944e959ebe3a
map111ks <map111ks@b932b21e-e20f-4f26-a6f4-977a90250f57>
2012-04-03 13:33:40 +0000

    Hausaufgabe nur Teilweise Kommentiert

--- Hausaufgaben_Martin_Petzold/Hausaufgabe12_Martin_Petzold/trunk/src/Hausaufgabe12/Box.java
+++ Hausaufgaben_Martin_Petzold/Hausaufgabe12_Martin_Petzold/trunk/src/Hausaufgabe12/Box.java
@@ -1,9 +1,82 @@
 package Hausaufgabe12;
+
 //Die Klasse die einen Karton erzeugt
 public class Box {
- private int width;//Die Breite des Kartons
- private int height;//Die Höhe des Kartons
- private 
- 
+	private double width;// Die Breite des Kartons
+	private double height;// Die Höhe des Kartons
+	private double length;// DIe Länge des Kartons
 
+	/*
+	 * Konstruktor zum erstellen eines Kartons
+	 */
+	public Box(double width, double heigth, double length) {
+		this.width = width;
+		this.height = heigth;
+		this.length = length;
+	}
+
+	/*
+	 * Konstruktor zur erstellung eines Würfelförmigen Kartons
+	 */
+	public Box(double side) {
+		this.width = this.height = this.length = side;
+	}
+
+	public Box(Box oldBox) {
+		this.width = oldBox.getWidth();
+		this.height = oldBox.getHeight();
+		this.length = oldBox.getLength();
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public double getLength() {
+		return length;
+	}
+
+	public void setLength(double length) {
+		this.length = length;
+	}
+
+	public double calculateVolume() {
+		return (width * height * length);
+	}
+
+	public double calculateSurfaceArea() {
+		return (2 * (width * height + width * length + height * length));
+	}
+
+	public Box getBiggerBox() {
+		Box biggerBox = new Box(this);
+		double faktor = 1.25;
+		biggerBox.setHeight(this.getHeight() * faktor);
+		biggerBox.setLength(this.getLength() * faktor);
+		biggerBox.setWidth(this.getWidth() * faktor);
+		return biggerBox;
+	}
+
+	public boolean FitsIn(Box otherbox) {
+		if (otherbox.getHeight() > this.getHeight()) {
+			if (otherbox.getLength() > this.getLength()) {
+				if (otherbox.getWidth() > this.getWidth()) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }
---
+++ Hausaufgaben_Martin_Petzold/Hausaufgabe12_Martin_Petzold/trunk/src/Hausaufgabe12test/test.java
@@ -0,0 +1,85 @@
+package Hausaufgabe12test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import Hausaufgabe12.Box;
+import org.junit.After;
+
+public class test {
+
+	private Box testbox;
+	@Before
+	public void setUp() throws Exception {
+		
+		testbox = new Box(2,5,7);
+	}
+	
+	@After
+	public void tearDown()
+	{
+		testbox = null;
+	}
+	@Test
+	public void testWuerfelKarton() {
+		double i = 10;
+		Box Wuerfel=new Box (i);
+		assertEquals("Die Übergabe für einen Würfelförmigen Karton im Wert Höhe ist nicht Korrekt",i, Wuerfel.getHeight(),0);
+		assertEquals("Die Übergabe für einen Würfelförmigen Karton im Wert Länge ist nicht Korrekt",i, Wuerfel.getLength(),0);
+		assertEquals("Die Übergabe für einen Würfelförmigen Karton im Wert Breite ist nicht Korrekt",i, Wuerfel.getWidth(),0);
+	}
+	@Test
+	public void testCalculateVolume() {
+		assertTrue("Die Volumenberechnung ist Fehlerhaft",testbox.calculateVolume()==70);
+	}
+
+	@Test
+	public void testCalculateSurfaceArea() {
+		assertTrue("Die Oberflächenberechnung ist Fehlerhaft",testbox.calculateSurfaceArea()==118);
+	}
+
+	@Test
+	public void testGetBiggerBox() {
+		assertFalse(testbox.getBiggerBox()==testbox);
+		assertTrue(testbox.getBiggerBox().getHeight()==testbox.getHeight()*1.25);
+		assertTrue(testbox.getBiggerBox().getLength()==testbox.getLength()*1.25);
+		assertTrue(testbox.getBiggerBox().getWidth()==testbox.getWidth()*1.25);
+	}
+
+	@Test
+	public void testFitsIn() {
+		Box testbox2 = new Box(2,5,7);
+		testbox2.setHeight(testbox.getHeight()+1);
+		testbox2.setLength(testbox.getLength()+1);
+		testbox2.setWidth(testbox.getWidth()+1);
+		assertTrue("Die FitsIn Methode ist Fehlerhaft",testbox.FitsIn(testbox2));
+		testbox2.setHeight(testbox.getHeight());
+		testbox2.setLength(testbox.getLength()+1);
+		testbox2.setWidth(testbox.getWidth()+1);
+		assertFalse("Die FitsIn Methode ist im Höhenvergleich Fehlerhaft",testbox.FitsIn(testbox2));
+		testbox2.setHeight(testbox.getHeight()+1);
+		testbox2.setLength(testbox.getLength());
+		testbox2.setWidth(testbox.getWidth()+1);
+		assertFalse("Die FitsIn Methode ist im Längenvergleich Fehlerhaft",testbox.FitsIn(testbox2));
+		testbox2.setHeight(testbox.getHeight()+1);
+		testbox2.setLength(testbox.getLength()+1);
+		testbox2.setWidth(testbox.getWidth());
+		assertFalse("Die FitsIn Methode ist im Breitenvergleich Fehlerhaft",testbox.FitsIn(testbox2));
+	}
+	public void testBoxUebergabe()
+	{
+		Box testbox2 = new Box(testbox);
+		assertFalse("Die neue Box ist kein eigenständiges Objekt",testbox==(testbox2));
+		assertEquals(testbox.getHeight(), testbox2.getHeight(),0);
+		assertEquals(testbox.getLength(), testbox2.getLength(),0);
+		assertEquals(testbox.getWidth(), testbox2.getWidth(),0);
+		testbox2.setHeight(1);
+		testbox2.setLength(1);
+		testbox2.setWidth(1);
+		assertFalse(testbox.getHeight()==testbox2.getHeight());
+		assertFalse(testbox.getLength()==testbox2.getLength());
+		assertFalse(testbox.getWidth()==testbox2.getWidth());
+		
+	}
+}
