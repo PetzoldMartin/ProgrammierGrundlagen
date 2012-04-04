d01fdd24eb5ba1363077de6d89d64389e0ba0f2f
map111ks <map111ks@b932b21e-e20f-4f26-a6f4-977a90250f57>
2012-04-04 17:53:19 +0000

    Software engeneering Praktikum fertig kommentiert

--- SEE/SEEP1/trunk/tests/de/fh_zwickau/se/MengeTest.java
+++ SEE/SEEP1/trunk/tests/de/fh_zwickau/se/MengeTest.java
@@ -6,12 +6,19 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * @author Aismael
+ *
+ */
 public class MengeTest {
 
 	Menge testMenge1;
 	Menge testMenge2;
 	int x, y, z, u, v;
 
+	/**
+	 * @throws Exception Initialisiert 2 Mengen un5 variablen von 1 bis 5
+	 */
 	@Before
 	public void setUp() throws Exception {
 		testMenge1 = new Menge();
@@ -23,12 +30,18 @@ public class MengeTest {
 		v = 5;
 	}
 
+	/**
+	 * @throws Exception Setzt die Mengen Null
+	 */
 	@After
 	public void tearDown() throws Exception {
 		testMenge1 = null;
 		testMenge2 = null;
 	}
 
+	/**
+	 * Testet ob der Konstruktor die Mengen korrekt initialisiert
+	 */
 	@Test
 	public void testMenge() {
 		assertNotNull("Die TestMenge1 wurde nicht angelegt ", testMenge1);
@@ -36,6 +49,9 @@ public class MengeTest {
 
 	}
 
+	/**
+	 * testet ob die addmethode die Elemente korrekt Hinzufügt
+	 */
 	@Test
 	public void testAdd() {
 		testMenge1.add(x);
@@ -44,23 +60,37 @@ public class MengeTest {
 
 	}
 
+	/**
+	 *  Testet ob die is Empty Methode Korrekt arbeitet
+	 */
 	@Test
 	public void testIsEmpty() {
 		assertTrue("die is empty Methode ist Fehlerhaft", testMenge1.isEmpty());
 		assertTrue("die is empty Methode ist Fehlerhaft", testMenge2.isEmpty());
 	}
 
+	/**
+	 * testet ob die Ellementzahl korrekt Wiedergegeben wird und
+	 * ob beim adden des selben Elements nur eins geschrieben wird
+	 */
 	@Test
 	public void testElementezahl() {
 		testMenge1.add(x);
-		assertEquals("Die Elementanzahl wird Falsch berechnet",
+		assertEquals("Die Elementanzahl wird bei einem Element Falsch berechnet",
 				testMenge1.elementezahl(), 1);
+		testMenge1.add(y);
+		assertEquals("Die Elementanzahl wird bei mehreren Elementen Falsch berechnet",
+				testMenge1.elementezahl(), 2);
 		testMenge1.add(x);
 		assertEquals(
-				"Die Elementanzahl wird Falsch berechnet:bei zweimal Gleicher Werteingabe",1,
+				"Die Elementanzahl wird Falsch berechnet:bei zweimal Gleicher Werteingabe",2,
 				testMenge1.elementezahl());
 	}
 
+	/**
+	 * Testet ob Elemente Korrekt gelöscht werden und
+	 * ob bei löschen eines nicht vorhandenen Elements ein Fehler Auftritt
+	 */
 	@Test
 	public void testDeleteElement() {
 		testMenge1.add(x);
@@ -75,6 +105,9 @@ public class MengeTest {
 
 	}
 
+	/**
+	 * Testet ob eingegebene Elemente Korrekt auf vorhandensein geprüft werden
+	 */
 	@Test
 	public void testIsElement() {
 		testMenge1.add(x);
@@ -82,6 +115,9 @@ public class MengeTest {
 
 	}
 
+	/**
+	 * Testet ob die Summenberechnung aller Elemente Korrekt ist
+	 */
 	@Test
 	public void testSumme() {
 		assertEquals("Die Summenberechnung für 0 Element ist Fehlerhaft", 0,
@@ -103,6 +139,9 @@ public class MengeTest {
 				testMenge1.summe());
 	}
 
+	/**
+	 * Testet ob der Durchschnittswert Korrekt berechnet wird
+	 */
 	@Test
 	public void testDurchschnittswert() {
 		testMenge1.add(x);
@@ -134,19 +173,35 @@ public class MengeTest {
 				(testMenge1.summe() / 2), testMenge1.durchschnittswert());
 	}
 
+	/**
+	 * Testet ob die Durchscnittsberechnung die Richtige Exeption Wirft
+	 * und sich danach erholt
+	 */
 	@Test
 	public void testDurchschnittswertBeiLeererMenge() {
 		try {
 			// testMenge1.add(x);
 			testMenge1.durchschnittswert();
-			fail("die Durchschnittswertberechnung für Leere Mengen ist fehlerhaft ");
+			fail("die Durchschnittswertberechnung für Leere Mengen ist fehlerhaft :MengeIstLeerException löst nicht aus");
 
 		} catch (MengeIstLeerException expected) {
+
+		}
+		try {
+			testMenge1.add(x);
+			assertEquals("die Durchschnittswertberechnung erholt sich nicht nach MengeIstLeerException ",1, testMenge1.durchschnittswert());
+			
+
+		} catch (MengeIstLeerException expected) {
+			fail("die Durchschnittswertberechnung erholt sich nicht nach MengeIstLeerException ");
 
 		}
 
 	}
 
+	/**
+	 * Testet ob Elemente Mit Bestimmter eigenschafft korrekt gezählt werden
+	 */
 	@Test
 	public void testZaehle_elemente_mit_bestimmter_eigenschaft() {
 		testMenge1.add(x);
@@ -167,6 +222,9 @@ public class MengeTest {
 
 	}
 
+	/**
+	 * Testet Grenzwerrte und die Größe des Arreys bis 100
+	 */
 	@Test
 	public void Grenzwerte() {
 		testMenge1.add(0);
@@ -192,9 +250,18 @@ public class MengeTest {
 
 	}
 
+	/**
+	 * Testet das Interface BewerteObGroesserdrei
+	 */
 	@Test
 	public void testBewerteObGroesserdrei() {
 		Bewerter bew = new BewerteObGroesserDrei();
+		assertFalse(
+				"der Bewerter Groesser Drei ist Fehlerhaft bei Werten unter Drei",
+				bew.bewerte(2));
+		assertTrue(
+				"der Bewerter Groesser Drei ist Fehlerhaft Bei Werten über Drei",
+				bew.bewerte(4));
 		assertFalse(
 				"der Bewerter Groesser Drei ist Fehlerhaft weil er vergleicht groeßergleich drei",
 				bew.bewerte(3));
