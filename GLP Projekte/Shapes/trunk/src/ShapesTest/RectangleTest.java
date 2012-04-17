ff0233ee4e835ad2794a945d25d3e01b3f3f9153
map111ks <map111ks@b932b21e-e20f-4f26-a6f4-977a90250f57>
2012-04-17 16:53:20 +0000

    Variablen umbenannt, equalmethoden und movemethoden implementiert und getestet, mainklasse umgebaut

--- GLP Projekte/Shapes/trunk/src/ShapesTest/RectangleTest.java
+++ GLP Projekte/Shapes/trunk/src/ShapesTest/RectangleTest.java
@@ -2,6 +2,9 @@ package ShapesTest;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,11 +13,15 @@ import Shapes.Point;
 import Shapes.Rectangle;
 
 public class RectangleTest {
-	Rectangle x,y;
+	Rectangle x,y,z;
+	ArrayList<Point> A1;
 	@Before
 	public void setUp() throws Exception {
 		x=new Rectangle(new Point(1, 1), 1, 1);
 		y=new Rectangle(new Point(1, 1), 1, 1);
+		z=new Rectangle(new Point(1, 1), 1, 1);
+		A1 = new ArrayList<Point>();
+		A1.add(new Point(1,1));
 	}
 
 	@After
@@ -25,6 +32,24 @@ public class RectangleTest {
 	public void test() {
 		assertTrue("Die equals Methode testet nicht auf Reflexivität",x.equals(x));
 		assertTrue("Die equals Methode testet nicht auf Symetrie",x.equals(y)&&y.equals(x));
+	}
+	@Test
+	public void testMove() {
+		assertTrue("Vortest vor move Fehlgeschlagen",x.equals(z));
+		assertEquals("Das Punkte Arrey wird falsch übergeben",z.getPoints(),A1);
+		assertEquals("die X Länge wurde falsch übergeben",z.getLx(), 1.0,0.0);
+		assertEquals("die Y Länge wurde falsch übergeben",z.getLy(), 1,0);
+		z.move(10, 10);
+		ArrayList<Point> A4 =A1;
+		for(Iterator <Point> iterator = A4.iterator(); iterator.hasNext();)
+        {
+            iterator.next().move(10,10);
+        }
+		assertEquals("Ein Punkt wurde falsch verschoben",z.getPoints(), A4);
+		assertEquals("die X Länge ist inkonsistent",z.getLx(), 1,0);
+		assertEquals("die Y Länge ist inkonsistent",z.getLy(), 1,0);
+		assertFalse("Die Polygone sind nicht unabhängig",x.equals(z));
+		
 	}
 
 }
