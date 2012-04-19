66502285a7a51da50a0044f2e2a5eecf899df9c0
map111ks <map111ks@b932b21e-e20f-4f26-a6f4-977a90250f57>
2012-04-19 13:11:10 +0000

    rm1.deleteRoom(R1) 2maliges Löschen???

--- GLP_Hausaufgaben_Martin_Petzold/Hausaugabe13_Martin_Petzold/trunk/src/TestGoodRoomManager.java
+++ GLP_Hausaufgaben_Martin_Petzold/Hausaugabe13_Martin_Petzold/trunk/src/TestGoodRoomManager.java
@@ -7,26 +7,39 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestGoodRoomManager {
-	RoomManager rm1,rm2,rm3,rm4,rm5,rm6,rm7;
-	Room R1,R2,R3,R4,R5,R6,R7;
-	ArrayList<Room> S1,S2,S3,S4,S5,S6,S7;
+	RoomManager rm1, rm2, rm3, rm4, rm5, rm6, rm7, rm8;
+	Room R1, R2, R3, R4, R5, R6, R7, R8;
+	ArrayList<Room> S1, S2, S3, S4, S5, S6, S7, S8;
+
 	@Before
 	public void setUp() throws Exception {
-		rm1= new GoodRoomManager();
-		rm2= new GoodRoomManager();
-		rm3= new GoodRoomManager();
-		rm4= new GoodRoomManager();
-		rm5= new GoodRoomManager();
-		rm6= new GoodRoomManager();
-		rm7= new GoodRoomManager();
-		R1 = new Room("100",true,16,true,"R",16);
-		R2 = new Room("100",false,16,true,"R",16);
-		R3 = new Room("100",true,17,true,"R",16);
-		R4 = new Room("100",true,16,false,"R",16);
-		R5 = new Room("100",true,16,true,"T",16);
-		R6 = new Room("100",true,16,true,"R",17);
+		rm1 = new GoodRoomManager();
+		rm2 = new GoodRoomManager();
+		rm3 = new GoodRoomManager();
+		rm4 = new GoodRoomManager();
+		rm5 = new GoodRoomManager();
+		rm6 = new GoodRoomManager();
+		rm7 = null;
+		R1 = new Room("100", true, 16, true, "R", 16);
+		R2 = new Room("100", false, 16, true, "R", 16);
+		R3 = new Room("100", true, 17, true, "R", 16);
+		R4 = new Room("100", true, 16, false, "R", 16);
+		R5 = new Room("100", true, 16, true, "T", 16);
+		R6 = new Room("100", true, 16, true, "R", 17);
 		R7 = null;
-		S1=new ArrayList<Room>();
+		S1 = new ArrayList<Room>();
+		S2 = new ArrayList<Room>();
+		S3 = new ArrayList<Room>();
+		S4 = new ArrayList<Room>();
+		S5 = new ArrayList<Room>();
+		S6 = new ArrayList<Room>();
+		S7 = null;
+		rm1.registerRoom(R1);
+		rm2.registerRoom(R2);
+		rm3.registerRoom(R3);
+		rm4.registerRoom(R4);
+		rm5.registerRoom(R5);
+		rm6.registerRoom(R6);
 	}
 
 	@After
@@ -35,52 +48,151 @@ public class TestGoodRoomManager {
 
 	@Test
 	public void testregisterRoom() {
-		assertFalse("Die RoomManager sind nicht voneinander Unabhängig",rm1.equals(rm2));
+		assertFalse("Die RoomManager sind nicht voneinander Unabhängig",
+				rm1.equals(rm2));
 		rm1.registerRoom(null);
-		rm1.registerRoom(new Room("100",true,16,true,"R",16));
-		for(Integer i=2;i<=1000;i++)
-		{
-			
+		rm1.registerRoom(R1);
+		for (Integer i = 2; i <= 1000; i++) {
+
 			try {
-				rm1.registerRoom(new Room(i.toString(),true,16,true,"R",16));
+				rm1.registerRoom(new Room(i.toString(), true, 16, true, "R", 16));
 
 			} catch (ArrayIndexOutOfBoundsException expected) {
 
 				fail("Der Room Manager kann nur " + i + " Rooms fassen");
 			}
 		}
-	
+
 	}
+
 	@Test
 	public void testgetMatchingRooms() {
-		rm1.registerRoom(R1);
-		rm2.registerRoom(R2);
-		rm3.registerRoom(R3);
-		rm4.registerRoom(R4);
-		rm5.registerRoom(R5);
-		rm6.registerRoom(R6);
-		rm7.registerRoom(R7);
+
 		S1.add(R1);
-		rm1.getMatchingRooms(true, 16);
-        assertEquals(rm1.getMatchingRooms(true, 16),S1);
-		
+		assertNull("Es können nicht erzeugte RoomManager Abgefragt werden", R7);
+		// Beamer Abfragen
+		assertEquals(
+				"Raum mit Beamer Abfrage bei vorhandenen Beamer ist nicht korrekt",
+				S1, rm1.getMatchingRooms(true, 15));
+		assertEquals(
+				"Raum ohne Beamer Abfrage bei vorhandenen Beamer ist nicht korrekt",
+				S1, rm1.getMatchingRooms(false, 15));
+		assertEquals(
+				"Raum mit Beamer Abfrage bei nicht vorhandenen Beamer ist nicht korrekt",
+				S3, rm2.getMatchingRooms(true, 15));
+		S2.add(R2);
+		assertEquals(
+				"Raum ohne Beamer Abfrage bei nicht vorhandenen Beamer ist nicht korrekt",
+				S2, rm2.getMatchingRooms(false, 15));
+		// Grenzwert Abfragen
+		assertEquals(
+				"Grenzwertabfrage bei Raumgröße ist im Maximalwert nicht korrekt",
+				S1, rm1.getMatchingRooms(true, 16));
+		assertEquals(
+				"Grenzwertabfrage bei Werten oberhalb der Grenze ist nicht korrekt",
+				S3, rm1.getMatchingRooms(true, 17));
+		assertEquals("Grenzwertabfrage bei Wert 0 ist nicht korrekt", S1,
+				rm1.getMatchingRooms(true, 0));
+		assertEquals("Grenzwertabfrage bei Wert -1 ist nicht korrekt", S1,
+				rm1.getMatchingRooms(true, -1));// ??? negative Plätze ???
+		// gemischte Abfragen Beamer und oberer Grenzwert
+		assertEquals(
+				"Raum mit Beamer Abfrage bei vorhandenen Beamer und oberen Grenzwert ist nicht korrekt",
+				S1, rm1.getMatchingRooms(true, 16));
+		assertEquals(
+				"Raum ohne Beamer Abfrage bei vorhandenen Beamer und oberen Grenzwert ist nicht korrekt",
+				S1, rm1.getMatchingRooms(false, 16));
+		assertEquals(
+				"Raum mit Beamer Abfrage bei nicht vorhandenen Beamer und oberen Grenzwert ist nicht korrekt",
+				S3, rm2.getMatchingRooms(true, 16));
+		assertEquals(
+				"Raum ohne Beamer Abfrage bei nicht vorhandenen Beamer und oberen Grenzwert ist nicht korrekt",
+				S2, rm2.getMatchingRooms(false, 16));
+		// gemischte Abfragen Beamer und oberhalb des Grenzwertes
+		assertEquals(
+				"Raum mit Beamer Abfrage bei vorhandenen Beamer und oberhalb des Grenzwertes ist nicht korrekt",
+				S3, rm1.getMatchingRooms(true, 17));
+		assertEquals(
+				"Raum ohne Beamer Abfrage bei vorhandenen Beamer und oberhalb des Grenzwertes ist nicht korrekt",
+				S3, rm1.getMatchingRooms(false, 17));
+		assertEquals(
+				"Raum mit Beamer Abfrage bei nicht vorhandenen Beamer und oberhalb des Grenzwertes ist nicht korrekt",
+				S3, rm2.getMatchingRooms(true, 17));
+		assertEquals(
+				"Raum ohne Beamer Abfrage bei nicht vorhandenen Beamer und oberhalb des Grenzwertes ist nicht korrekt",
+				S3, rm2.getMatchingRooms(false, 17));
+
 	}
 
-
+	 @Test
+	 public void testdeleteRoom() {
+	 rm1.registerRoom(R1);
+	 // assertTrue(rm1.deleteRoom(R1));
+	 rm1.deleteRoom(R1);
+	 assertEquals("Ein vorhandener Raum konnte nicht gelöscht werden",S1,
+	 rm1.getMatchingRooms(false, 12));
+	
+	 }
 	@Test
 	public void testgetMatchingComputerPools() {
-		fail("Not yet implemented");
+		S1.add(R1);
+		assertNull("Es können nicht erzeugte RoomManager Abgefragt werden", R7);
+		// Beamer Abfragen
+		assertEquals(
+				"Computerpool mit Beamer Abfrage bei vorhandenen Beamer ist nicht korrekt",
+				S1, rm1.getMatchingComputerPools(true,0, 15));
+		assertEquals(
+				"Raum ohne Beamer Abfrage bei vorhandenen Beamer ist nicht korrekt",
+				S1, rm1.getMatchingComputerPools(false,0, 15));
+		assertEquals(
+				"Raum mit Beamer Abfrage bei nicht vorhandenen Beamer ist nicht korrekt",
+				S3, rm2.getMatchingComputerPools(true,0, 15));
+		S2.add(R2);
+		assertEquals(
+				"Raum ohne Beamer Abfrage bei nicht vorhandenen Beamer ist nicht korrekt",
+				S2, rm2.getMatchingComputerPools(false,0, 15));
+		// Grenzwert Abfragen
+		assertEquals(
+				"Grenzwertabfrage bei Raumgröße ist im Maximalwert nicht korrekt",
+				S1, rm1.getMatchingComputerPools(true,0, 16));
+		assertEquals(
+				"Grenzwertabfrage bei Werten oberhalb der Grenze ist nicht korrekt",
+				S3, rm1.getMatchingComputerPools(true,0, 17));
+		assertEquals("Grenzwertabfrage bei Wert 0 ist nicht korrekt", S1,
+				rm1.getMatchingComputerPools(true,0, 0));
+		assertEquals("Grenzwertabfrage bei Wert -1 ist nicht korrekt", S1,
+				rm1.getMatchingComputerPools(true,0, -1));// ??? negative Plätze ???
+		// gemischte Abfragen Beamer und oberer Grenzwert
+		assertEquals(
+				"Raum mit Beamer Abfrage bei vorhandenen Beamer und oberen Grenzwert ist nicht korrekt",
+				S1, rm1.getMatchingComputerPools(true,0, 16));
+		assertEquals(
+				"Raum ohne Beamer Abfrage bei vorhandenen Beamer und oberen Grenzwert ist nicht korrekt",
+				S1, rm1.getMatchingComputerPools(false,0, 16));
+		assertEquals(
+				"Raum mit Beamer Abfrage bei nicht vorhandenen Beamer und oberen Grenzwert ist nicht korrekt",
+				S3, rm2.getMatchingComputerPools(true,0, 16));
+		assertEquals(
+				"Raum ohne Beamer Abfrage bei nicht vorhandenen Beamer und oberen Grenzwert ist nicht korrekt",
+				S2, rm2.getMatchingComputerPools(false,0, 16));
+		// gemischte Abfragen Beamer und oberhalb des Grenzwertes
+		assertEquals(
+				"Raum mit Beamer Abfrage bei vorhandenen Beamer und oberhalb des Grenzwertes ist nicht korrekt",
+				S3, rm1.getMatchingComputerPools(true,0, 17));
+		assertEquals(
+				"Raum ohne Beamer Abfrage bei vorhandenen Beamer und oberhalb des Grenzwertes ist nicht korrekt",
+				S3, rm1.getMatchingComputerPools(false,0, 17));
+		assertEquals(
+				"Raum mit Beamer Abfrage bei nicht vorhandenen Beamer und oberhalb des Grenzwertes ist nicht korrekt",
+				S3, rm2.getMatchingComputerPools(true,0, 17));
+		assertEquals(
+				"Raum ohne Beamer Abfrage bei nicht vorhandenen Beamer und oberhalb des Grenzwertes ist nicht korrekt",
+				S3, rm2.getMatchingComputerPools(false,0, 17));
 	}
 
-	@Test
-	public void testgetMatchingRoomInABuilding() {
-		fail("Not yet implemented");
-	}
-	@Test
-	public void testdeleteRoom() {
-		fail("Not yet implemented");
-	}
-
-	
+	// @Test
+	// public void testgetMatchingRoomInABuilding() {
+	// fail("Not yet implemented");
+	// }
 
 }
