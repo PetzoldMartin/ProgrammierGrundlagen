99899a2905d45ba7bd52ec0161ecdd9694984f10
map111ks <map111ks@b932b21e-e20f-4f26-a6f4-977a90250f57>
2012-05-18 19:26:18 +0000

    ad new

---
+++ AD/src/Praktikum1/Praktikum1_1.java
@@ -0,0 +1,15 @@
+package Praktikum1;
+
+
+
+public class Praktikum1_1 extends Praktikum1.Zeileneinleser {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		einlesen();
+
+	}
+
+}
