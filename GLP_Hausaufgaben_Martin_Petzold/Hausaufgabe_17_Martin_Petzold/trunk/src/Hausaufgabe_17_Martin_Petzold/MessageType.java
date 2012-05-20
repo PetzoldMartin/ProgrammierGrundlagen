19043c10a5beb9309f8855e881904f1c9b95529d
map111ks <map111ks@b932b21e-e20f-4f26-a6f4-977a90250f57>
2012-05-20 19:46:49 +0000

    „Endgültige Version. Bitte kontrollieren.“

--- GLP_Hausaufgaben_Martin_Petzold/Hausaufgabe_17_Martin_Petzold/trunk/src/Hausaufgabe_17_Martin_Petzold/MessageType.java
+++ GLP_Hausaufgaben_Martin_Petzold/Hausaufgabe_17_Martin_Petzold/trunk/src/Hausaufgabe_17_Martin_Petzold/MessageType.java
@@ -5,7 +5,8 @@ package Hausaufgabe_17_Martin_Petzold;
  * @author Matin Petzold
  * Mit dem Schlüsselwort enum können Aufzählungen eingeleitet werden.
  * Das heisst das zu jedem in Enum Definiertem Typ ein Statisches Finales Objekt erstellt wird,
- *  wobei dabei der Name als String und die Stelle als Integer abgespeichert wird
+ *  Insbesondere wird mit der Deklaration dieser Finalen Objekte ein neuer Datetyp Deklariert.
+ *  Diese können wiederum übersichtlich in switch Konstrukten verwendet werden
  */
 
 public enum MessageType {
